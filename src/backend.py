1060502b7e48f4a780d92ca18b2fb272d02a7c3d
Lianming Du <adullb@qq.com>
2024-11-27 22:38:05 +0800

    update

--- src/backend.py
+++ src/backend.py
@@ -6,94 +6,15 @@ from utils import *
 
 __all__ = ['RDB', 'SqlQuery']
 
-class DataBackend(QObject):
-	error = Signal(str)
-	mutex = QMutex()
-	conn = None
-
-	def __init__(self, parent=None):
-		super().__init__(parent)
-
-	def __del__(self):
-		if self.conn is not None:
-			self.conn.close()
-
-	def connect(self, file=':memory:'):
-		if self.conn is not None:
-			self.conn.close()
-
-		self.conn = sqlite3.connect(file)
-
-	@property
-	def cursor(self):
-		self.mutex.lock()
-		cur = self.conn.cursor()
-		self.mutex.unlock()
-		return cur
-
-	def query(self, sql, args=None):
-		if args:
-			return self.cursor.execute(sql, args)
-		else:
-			return self.cursor.execute(sql)
-
-	def get_one(self, sql, *args):
-		for row in self.query(sql, args):
-			return row[0]
-
-	def get_row(self, sql, *args):
-		for row in self.query(sql, args):
-			return row
-
-	def get_rows(self, sql, *args):
-		for row in self.query(sql, args):
-			yield row
-
-	def get_column(self, sql, *args):
-		return [row[0] for row in self.query(sql, args)]
-
-	def get_dict(self, sql, *args):
-		res = self.query(sql, args)
-		fields = [col[0] for col in res.description]
-
-		for row in res:
-			return AttrDict(zip(fields, row))
-
-	def get_dicts(self, sql, *args):
-		res = self.query(sql, args)
-		fields = [col[0] for col in res.description]
-
-		for row in res:
-			yield AttrDict(zip(fields, row))
-
-	def get_fields(self, table):
-		sql = "SELECT * FROM {} LIMIT 1".format(table)
-		res = self.query(sql)
-		return [col[0] for col in res.description]
-	
-	def insert(self, table, data):
-		field_num = len(self.get_fields(table))
-		sql = "INSERT INTO {} VALUES ({})".format(
-			table,
-			','.join(['?']*field_num)
-		)
-		self.cursor.executemany(sql, data)
-
-	def delete(self, table, rowid):
-		sql = "DELETE FROM {} WHERE id=?".format(table)
-		self.query(sql, (rowid,))
-
-	def update(self, table, rowid, **kwargs):
-		keys = []
-		vals = []
-
-		for k, v in kwargs.items():
-			keys.append("{}=?".format(k))
-			vals.append(v)
-
-		vals.append(rowid)
-		sql = "UPDATE {} SET {} WHERE id=?".format(table, ','.join(keys))
-		self.query(sql, vals)
+data_tables = {
+	'input': [
+		'id INTEGER PRIMARY KEY',
+		'type TEXT',
+		'content TEXT'
+	],
+	'output': [
+	]
+}
 
 class SqlQuery:
 	def __init__(self, table):
@@ -101,20 +22,29 @@ class SqlQuery:
 		self._selects = []
 		self._updates = []
 		self._inserts = []
+		self._creates = []
 		self._and_wheres = []
 		self._or_wheres = []
 		self._order_bys = []
-		self._order_desc = False
+		self._order_asc = True
 		self._limit = []
 		self._offset = []
 		self._action = None
 		self._querys = []
+
+	def __str__(self):
+		return self.build()
 
 	def __add(self, item):
 		if not self._querys:
 			self._querys.append(self._action)
 
 		self._querys.extend([' ', item])
+
+	def create(self, *args):
+		self._creates = args
+		self._action = 'CREATE'
+		return self
 
 	def select(self, *args):
 		self._selects.extend(args)
@@ -156,8 +86,15 @@ class SqlQuery:
 		self._offset = num
 		return self
 
+	def first(self):
+		self._limit = 1
+		return self
+
 	def build(self):
 		match self._action:
+			case 'CREATE':
+				self.__add("TABLE ({})".format(','.join(self._creates)))
+
 			case 'SELECT':
 				if self._selects:
 					self.__add(','.join(self._selects))
@@ -211,6 +148,77 @@ class SqlQuery:
 
 		return ''.join(self._querys)
 
+class DataBackend(QObject):
+	error = Signal(str)
+	mutex = QMutex()
+	conn = None
+
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+	def __del__(self):
+		if self.conn is not None:
+			self.conn.close()
+
+	def connect(self, file=':memory:'):
+		if self.conn is not None:
+			self.conn.close()
+
+		self.conn = sqlite3.connect(file)
+
+	@property
+	def cursor(self):
+		self.mutex.lock()
+		cur = self.conn.cursor()
+		self.mutex.unlock()
+		return cur
+
+	def query(self, sql, args=None):
+		if args:
+			return self.cursor.execute(str(sql), args)
+		else:
+			return self.cursor.execute(str(sql))
+
+	def get_one(self, sql, *args):
+		for row in self.query(sql, args):
+			return row[0]
+
+	def get_row(self, sql, *args):
+		for row in self.query(sql, args):
+			return row
+
+	def get_rows(self, sql, *args):
+		for row in self.query(sql, args):
+			yield row
+
+	def get_column(self, sql, *args):
+		return [row[0] for row in self.query(sql, args)]
+
+	def get_dict(self, sql, *args):
+		res = self.query(sql, args)
+		fields = [col[0] for col in res.description]
+
+		for row in res:
+			return AttrDict(zip(fields, row))
+
+	def get_dicts(self, sql, *args):
+		res = self.query(sql, args)
+		fields = [col[0] for col in res.description]
+
+		for row in res:
+			yield AttrDict(zip(fields, row))
+
+	def has_table(self, table):
+		sql = SqlQuery('sqlite_master')\
+			.select('name')\
+			.where('type=?', 'name=?')\
+			.first()
+		res = self.get_one(sql, ('table', table))
+		return True if res else False
+
+	def get_fields(self, table):
+		sql = SqlQuery(table).select().one()
+		res = self.query(sql)
+		return [col[0] for col in res.description]
 
 RDB = DataBackend()
-
--- src/models.py
+++ src/models.py
@@ -45,44 +45,135 @@ class RNASuitePandasModel(QAbstractTableModel):
 
 		return None
 
-	'''
-	def canFetchMore(self, parent=QModelIndex()):
-		return self.display_count < len(self._data)
-
-	def fetchMore(self, parent=QModelIndex()):
-		add_count = 100
-
-		if self.display_count + add_count > len(self._data):
-			add_count = len(self._data) - self.display_count
-
-		self.beginInsertRows(QModelIndex(), self.display_count, self.display_count+add_count-1)
-		self.display_count += add_count
-		self.endInsertRows()
-	'''
-
-	"""
-	def sort(self, column, order):
-		if self._data.empty:
-			return
-
-		if order ==  Qt.DescendingOrder:
-			ascending = False
-		else:
-			ascending = True
-
-		self.beginResetModel()
-		self._data.sort_values(self._data.columns[column],
-			ascending = ascending,
-			inplace = True
-		)
-		self.endResetModel()
-	"""
-
 	def load_data(self, data):
 		self.beginResetModel()
 		#self.display_count = 10
 		self._data = data
 		self.endResetModel()
+
+class RNASuiteSqliteTableModel(QAbstractTableModel):
+	row_count = Signal(int)
+	col_count = Signal(int)
+	header_name = []
+	table_name = None
+
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.displays = []
+		self.selected = []
+		
+		self.total_count = 0
+		self.read_count = 0
+		self.read_size = 200
+
+		self.cache_data = {}
+
+	def rowCount(self, parent=QModelIndex()):
+		if parent.isValid():
+			return 0
+
+		return len(self.displays)
+
+	def columnCount(self, parent=QModelIndex()):
+		if parent.isValid():
+			return 0
+
+		count = len(self.header_name)
+		self.row_count.emit(count)
+		return count
+
+	def data(self, index, role=Qt.DisplayRole):
+		if not index.isValid():
+			return None
+
+		row = index.row()
+		col = index.column()
+
+		if role == Qt.DisplayRole:
+			return self.datasets[row][col]
+
+	def headerData(self, section, orientation, role=Qt.DisplayRole):
+		if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+			return self.header_title[section]
+
+	def canFetchMore(self, parent):
+		if parent.isValid():
+			return False
+
+		if self.read_count < self.total_count:
+			return True
+
+		return False
+
+	def fetchMore(self, parent):
+		if parent.isValid():
+			return
+
+		ids = DB.get_column(self.read_sql)
+		fetch_count = len(ids)
+		fetch_end = self.read_count+fetch_count-1
+		self.beginInsertRows(QModelIndex(), self.read_count, fetch_end)
+		self.displays.extend(ids)
+		self.read_count += fetch_count
+		self.endInsertRows()
+
+	@property
+	def count_sql(self):
+		return SqlQuery(self.table_name)\
+			.select('COUNT(1)')\
+			.first()
+
+	@property
+	def read_sql(self):
+		remain_count = self.total_count - self.read_count
+		fetch_count = min(self.read_size, remain_count)
+		return SqlQuery(self.table_name)\
+			.select('id')\
+			.limit(fetch_count)\
+			.offset(self.read_count)
+
+	@property
+	def get_sql(self):
+		return SqlQuery(self.table_name)\
+			.select('id')\
+			.first()
+
+	def update_cache(self, row):
+		row_id = self.displays[row]
+		self.cache_data ={row: RDB.get_row(self.get_sql, row_id)}
+
+	def get_value(self, row, col):
+		if row not in self.cache_data:
+			self.update_cache(row)
+
+		return self.cache_data[row][col]
+
+	def update(self):
+		self.beginResetModel()
+		self.read_count = 0
+		self.selected = []
+		self.total_count = RDB.get_one(self.count_sql)
+		self.displays = RDB.get_column(self.read_sql)
+		self.read_count = len(self.displays)
+		self.cache_data = {}
+		self.endResetModel()
+		self.row_count.emit(self.total_count)
+
+	def reset(self):
+		self.beginResetModel()
+		self.cache_data = {}
+		self.read_count = 0
+		self.displays = []
+		self.selected = []
+		self.total_count = 0
+		self.endResetModel()
+
+	def clear(self):
+		sql = SqlQuery(self.table_name).delete()
+		RDB.query(sql)
+		self.reset()
+
 
 class RNASuiteOutputTreeModel(QAbstractTableModel):
 	_headers = ['Name', '', 'plot', 'id', 'type', 'pyid']
